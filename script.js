fd4c1ef0c01c3bc926efc5fc9ff4c3115ca8543e
Suyash Kadam <122480150+SuyashGaneshKadam@users.noreply.github.com>
2024-01-18 00:52:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function daysOfAYear(year){
+	if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+        return 366;
+    } else {
+        return 365;
+    }
+}
